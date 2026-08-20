694554b4980419ca7b516559a091e9caea28e836
shivam-tamboli <123985867+shivam-tamboli@users.noreply.github.com>
2026-08-20 23:50:29 +0530

    Add Easy Java Solution from LeetCode

--- LeetCode/Java/Easy/349. Intersection of Two Arrays/Solution.java
+++ LeetCode/Java/Easy/349. Intersection of Two Arrays/Solution.java
@@ -1,30 +1,41 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
 
-        HashSet<Integer> map = new HashSet<>();
+        HashSet<Integer> map1 = new HashSet<>();
+
+        HashSet<Integer> map2 = new HashSet<>();
 
         HashSet<Integer> result = new HashSet<>();
 
-       for(int i = 0; i < nums1.length; i++){
-            map.add(nums1[i]);
+        
+
+        for(int num : nums1){
+            map1.add(num);
+        }
+
+        for(int  num : nums2){
+            map2.add(num);
+        }
+
+        for(int num : nums1){
+            if(map2.contains(num)){
+                result.add(num);
+            }
+        }
+
+       int[] ft = new int[result.size()];
+
+       int index = 0;
+       for(int num : result){
+        ft[index] = num;
+        index++;
        }
 
-       for(int i = 0; i < nums2.length; i++){
-        if(map.contains(nums2[i])){
-                result.add(nums2[i]);
-        }    
-       }
+       return ft;
 
-       int[] arr = new int[result.size()];
+    }    
 
-       int i = 0;
-
-       for(int x : result){
-         arr[i] = x;
-        i++;
-       }
-
-       return arr;
-     
-    }
 }
+    
+        
+    
