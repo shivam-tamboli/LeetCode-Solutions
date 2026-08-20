122087dc69cece92ce6651f010e23ff0dd62e45f
shivam-tamboli <123985867+shivam-tamboli@users.noreply.github.com>
2026-08-20 16:08:25 +0530

    Add Easy Java Solution from LeetCode

--- LeetCode/Java/Easy/242. Valid Anagram/Solution.java
+++ LeetCode/Java/Easy/242. Valid Anagram/Solution.java
@@ -1,27 +1,26 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
 
-        HashMap<Character, Integer> map = new HashMap<>();
-
         HashMap<Character, Integer> maps = new HashMap<>();
 
-        char[] cc = s.toCharArray();
-        char[] hh = t.toCharArray();
+        HashMap<Character, Integer> mapt = new HashMap<>();
 
-        for(char c : cc ){
-            map.put(c , map.getOrDefault(c, 0) + 1);
+        char[] ss = s.toCharArray();
+        char[] tt = t.toCharArray();
+
+        for(int i = 0; i < ss.length; i++){
+            maps.put(ss[i], maps.getOrDefault(ss[i], 0) + 1);
         }
 
-        for(char h : hh ){
-            maps.put(h , maps.getOrDefault(h, 0) + 1);
+        for(int i = 0; i < tt.length; i++){
+            mapt.put(tt[i], mapt.getOrDefault(tt[i], 0) + 1);
         }
- 
-        if(maps.equals(map)){
+
+        if(maps.equals(mapt)){
             return true;
-        }else{
-            return false;
         }
 
+        return false;
         
     }
 }
